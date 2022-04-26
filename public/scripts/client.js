a2ae124c60bdececc4f6cdb360bf251c0fdc0907
lamew128 <lamew128@gmail.com>
2022-04-26 15:54:01 -0400

    added counter function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,4 +3,26 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+/*
 
+document.addEventListener("input", (event) => {
+  console.log(event);
+  // your code here
+});
+
+
+document.addEventListener("keypress", (event) => {
+  console.log(event);
+  // your code here
+});
+
+document.addEventListener("keyup", (event) => {
+  console.log(event);
+  // your code here
+});
+
+document.addEventListener("keydown", (event) => {
+  console.log(event);
+  // your code here
+});
+*/
---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  // --- our code goes here ---
+  $("#tweet-text").on('input', function() {
+    //console.log($("#tweet-text").val().length);
+    $("#counter").html(() => {
+      let num = 140 - $("#tweet-text").val().length;
+      if(num <= 0) {
+        $("#counter").removeClass("counter");
+        $("#counter").addClass("counterZero");
+      } else {
+        $("#counter").removeClass("counterZero");
+        $("#counter").addClass("counter");
+      }
+        
+      return num;
+    });
+  });
+});
